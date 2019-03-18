4ba1df8d173fdbf34555ec34fa5980797cc0de92
daliborsimic <appleiMac22x?!>
2019-03-18 14:47:09 +0100

    Small refactoring + printing of comments

--- redditAPI.py
+++ redditAPI.py
@@ -1,21 +1,45 @@
 import praw
+import sys
 
 reddit = praw.Reddit(client_id='QS278a4Z1eWFBw',
                      client_secret='xvA8ljlQX6sV_sg2TBtTHsjcRd8',
                      user_agent='Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-en) AppleWebKit/533.19.4 (KHTML, like Gecko) Version/5.0.3 Safari/533.19.4')
 
 
-subreddit = reddit.subreddit('netflix')
+def showReplies(replies):
+    if not replies:
+        print('Replies:')
+        for reply in replies:
+            # print('\t' + reply)
+            print(vars(reply))
+            print('--------------')
+            if not reply.replies:
+                showReplies(reply.replies)
+        print('End Replies -----')
 
-# assume you have a Subreddit instance bound to variable `subreddit`
-for submission in subreddit.hot(limit=10):
-	#print('--------------')
-    #print(submission.title)  # Output: the submission's title
-    #print(submission.score)  # Output: the submission's score
-    #print(submission.id)     # Output: the submission's ID
-    #print(submission.url)    # Output: the URL the submission points to
-    #print(submission.comments)
-    #print('--------------')  # or the submission's URL if it's a self post
-	for top_level_comment in submission.comments:
-	    print(top_level_comment.body)
-	    print('--------------')
+
+def printSubreddit(subredditName, limit=10):
+    subreddit = reddit.subreddit(subredditName)
+
+    for submission in subreddit.hot(limit=limit):
+        print('Title: ' + submission.title)
+        print('Selftext: ' + submission.selftext)
+        print('Score: ' + str(submission.score))
+        print('ID: ' + str(submission.id))
+        print('URL: ' + submission.url)
+        print('Comments:')
+        submission.comments.replace_more(limit=None)
+        for comment in submission.comments.list():
+            print("Parent ID: " + str(comment.parent_id))
+            print(comment.body)
+            print('-------')
+        print('--------------')
+        print('\n\n\n')
+
+
+def main():
+    printSubreddit('netflix')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
