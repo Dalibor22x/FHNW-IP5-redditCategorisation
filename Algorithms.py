025db39a5b547c0724a25e567d59158088c7b2cc
daliborsimic <appleiMac22x?!>
2019-08-02 16:56:40 +0200

    Renamed variables

--- Algorithms.py
+++ Algorithms.py
@@ -86,36 +86,36 @@ class Algorithms:
         return algorithm
 
     @staticmethod
-    def hyperparameter_tuning__multinomial_naive_bayes(documents, text_model):
+    def hyperparameter_tuning__multinomial_naive_bayes(documents, feature_model):
         param_grid = {
             'alpha': [1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 1.1, 1.2, 1.3, 1.7, 2, 6, 3],
             'fit_prior': [True, False]
         }
 
         model = MultinomialNB()
-        Algorithms.hyperparameter_tuning(model, param_grid, documents, text_model)
+        Algorithms.hyperparameter_tuning(model, param_grid, documents, feature_model)
 
     @staticmethod
-    def hyperparameter_tuning__svc(documents, text_model):
+    def hyperparameter_tuning__svc(documents, feature_model):
         param_grid = {
             'gamma': [pow(2, -3), pow(2, -3.25), pow(2, -3.5), pow(2, -3.75), pow(2, -4), pow(2, -2.75), pow(2, -2.5), pow(2, -2.25), pow(2, -2), pow(2, -1.75)],
             'C': range(1, 15)
         }
 
         model = SVC()
-        Algorithms.random_hyperparameter_tuning(model, param_grid, documents, text_model)
+        Algorithms.random_hyperparameter_tuning(model, param_grid, documents, feature_model)
 
     @staticmethod
-    def hyperparameter_tuning__k_neighbors(documents, text_model):
+    def hyperparameter_tuning__k_neighbors(documents, feature_model):
         param_grid = {
             'n_neighbors': range(1, 31)
         }
 
         model = KNeighborsClassifier()
-        Algorithms.hyperparameter_tuning(model, param_grid, documents, text_model)
+        Algorithms.hyperparameter_tuning(model, param_grid, documents, feature_model)
 
     @staticmethod
-    def hyperparameter_tuning__random_forest(documents, text_model):
+    def hyperparameter_tuning__random_forest(documents, feature_model):
         param_grid = {
             'bootstrap': [False],
             'max_depth': [80, 90, 100, 110, 120],
@@ -126,16 +126,16 @@ class Algorithms:
         }
 
         model = RandomForestClassifier()
-        Algorithms.hyperparameter_tuning(model, param_grid, documents, text_model)
+        Algorithms.hyperparameter_tuning(model, param_grid, documents, feature_model)
 
     @staticmethod
-    def hyperparameter_tuning(model, param_grid, documents, text_model):
+    def hyperparameter_tuning(model, param_grid, documents, feature_model):
         grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=10, n_jobs=-1, verbose=2)
 
         docs = list(map(lambda x: ' '.join(x[0]), documents))
         y = list(map(lambda x: x[1], documents))
 
-        if text_model == "Bag of Words":
+        if feature_model == "Bag of Words":
             vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
             X = vectorizer.fit_transform(docs)
             X = X.toarray()
@@ -149,13 +149,13 @@ class Algorithms:
         print(grid.best_params_)
 
     @staticmethod
-    def random_hyperparameter_tuning(model, param_grid, documents, text_model):
+    def random_hyperparameter_tuning(model, param_grid, documents, feature_model):
         grid = RandomizedSearchCV(estimator=model, param_distributions=param_grid, cv=10, n_jobs=-1, verbose=2)
 
         docs = list(map(lambda x: ' '.join(x[0]), documents))
         y = list(map(lambda x: x[1], documents))
 
-        if text_model == "Bag of Words":
+        if feature_model == "Bag of Words":
             vectorizer = CountVectorizer(max_features=1500, min_df=5, max_df=0.7, stop_words=stopwords.words('english'))
             X = vectorizer.fit_transform(docs)
             X = X.toarray()
